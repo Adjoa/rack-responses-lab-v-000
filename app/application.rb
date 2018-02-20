64d6eb3305d65c3e4c939483b7df434967dd9ceb
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-20 14:21:39 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,13 @@
 class Application
   def call(env)
+    resp = Rack::Response.new
 
+    if condition 
+      resp.write "Morning"
+    else
+      resp.write "Afternoon"
+    end
+
+    resp.finish
   end
 end
